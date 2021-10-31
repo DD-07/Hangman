5d2eb69de90671fe17e7b5a8b4aaaf3490dcf71c
DT <69886702+DD-07@users.noreply.github.com>
2021-10-31 20:40:59 +0800

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,1 +1,6 @@
+# Create list with all the hangman content
+#create function to randomly choose the hangman content
+# create function to see if the letter is present in the list
+# create a function to see whether the player wins
+
 
